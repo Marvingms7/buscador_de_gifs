f0df51a5c6e96415409f7c9475a6cea44fc68f9a
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-10-04 15:50:33 -0300

    Update home_page.dart

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,7 +9,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  _getGifs() async {}
+  late String _buscar;
+
+  _getGifs() async {
+    http.Response response;
+
+    if (_buscar == null) {
+      response = await http.get(Uri.parse(
+          'https://api.giphy.com/v1/gifs/trending?api_key=aaVClH3IDxPutSQeutFyZrw8OXYS1wHA&limit=25&rating=g'));
+    } else {
+      response = await http.get(Uri.parse(
+          'https://api.giphy.com/v1/gifs/search?api_key=aaVClH3IDxPutSQeutFyZrw8OXYS1wHA&q=$_buscar&limit=25&offset=75&rating=g&lang=pt'));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
